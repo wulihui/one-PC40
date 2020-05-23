bdb709537d9cf560445d6cc5ddfdf8a5e9ed92e6
xiaoyueyue <me@ffff.com>
2020-05-23 22:08:07 +0800

    将数据提交到服务器

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -11,12 +11,16 @@
           <el-input v-model="form.mobile" placeholder="请输入手机号"></el-input>
         </el-form-item>
         <el-form-item prop="code">
+            <!-- <div class="myitem">
+                <el-input v-model="form.code" placeholder="请输入密码"></el-input>
+                <el-button class="itembtn">获取验证码</el-button>
+            </div> -->
           <el-row>
               <el-col :span="14">
                   <el-input v-model="form.code" placeholder="请输入密码"></el-input>
               </el-col>
-              <el-col :span="8">
-                  <el-button>获取验证码</el-button>
+              <el-col :span="8" :offset="2">
+                  <el-button class="colbtn">获取验证码</el-button>
               </el-col>
           </el-row>
         </el-form-item>
@@ -29,6 +33,9 @@
 </template>
 
 <script>
+// 导入axios
+import axios from 'axios'
+
 export default {
   data () {
     return {
@@ -49,13 +56,35 @@ export default {
     }
   },
   methods: {
+    // 用户的登录
     login () {
       this.$refs.form.validate(valid => {
         if (valid) {
-          console.log('通过')
+          this.submitData()
         } else {
           console.log('不通过')
         }
+      })
+    },
+    // 数据的提交
+    submitData () {
+      // 发送请求到服务器
+      axios({
+        url: 'http://ttapi.research.itcast.cn/mp/v1_0/authorizations',
+        method: 'post',
+        data: this.form
+      }).then(res => {
+        // res 中有一个属性叫做data,在data 中有两个属性后面我们会用上: token , refresh_token
+        // 只要进入到这个方法中说明登录成功
+        // 跳转到主页
+        this.$router.push('/')
+        this.$message({
+          message: '登录成功',
+          type: 'success'
+        })
+      }).catch(err => {
+        this.$message.error('手机号码或者验证码错误')
+        console.log(err)
       })
     }
   }
@@ -81,6 +110,16 @@ export default {
             }
         }
         .loginBtn {
+            width: 100%;
+        }
+        .myitem {
+            display: flex;
+            justify-content: center;
+            .itembtn {
+                margin-left: 20px;
+            }
+        }
+        .colbtn {
             width: 100%;
         }
     }
