073269addfb894ae0fbd53fb39828cb6caabc871
xiaoyueyue <me@ffff.com>
2020-05-23 14:05:31 +0800

    项目结构的搭建

---
+++ src/views/useElement/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="useElement">
+      <h3>
+          这是一个 element-ui
+      </h3>
+      <h4>
+        使用 element-ui 按钮
+      </h4>
+      <el-switch
+        v-model="value"
+        active-color="#13ce66"
+        inactive-color="#ff4949">
+      </el-switch>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      value: true
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
